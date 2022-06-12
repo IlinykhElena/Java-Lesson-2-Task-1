788494eb9bd6116a03409b64e3a996b50b03526b
ElenaIlinykh <bengaliki22@gmail.com>
2022-06-12 22:08:15 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 90;
+        int rublesForMile = 20;
+        int miles = price / rublesForMile;
+        System.out.println("Количество начисленных миль: " + miles);
+    }
+}
